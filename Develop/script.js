92929d22c03e20479ff2f26bb3196829d065db8c
Lizette Leon <lizetteleon@Lizettes-Air.attlocal.net>
2020-06-07 16:02:49 -0700

    prompts for adding upper case, lowercase, numbers and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,33 @@
 
 var characters = prompt("How many characters would you like your password to be?")
 
+if (characters < 8 || characters > 128){
+    alert("Length must be 8-128 characters")
+}
+
+if (characters >= 8 && characters <= 128){
+var uppercase = confirm("Would you like to use uppercase letters?")
+var lowercase = confirm("Would you like to use lowercase letters?")
+var numbers = confirm("would you like to use numbers?")
+var symbols = confirm("would you like to use special characters?")
+}    
+
+if (uppercase != true && lowercase != true && numbers != true && symbols != true){
+    alert("You must select at least one character type!")
+}
+
+//DOM elements
+const resultEl = document.getElementById('password');
+
+
+document.getElementById('generate').addEventListener('click', () => {	
+	const hasLower = lowercase.true;
+	const hasUpper = uppercase.true;
+	const hasNumber = numbers.true;
+	const hasSymbol = symbols.true;
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
 
 
 // Get references to the #generate element
@@ -11,6 +38,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
 
 
