34c1e1a683a9e8c0154d4b110b1f5627f2fe953d
Lizette Leon <lizetteleon@Lizettes-Air.attlocal.net>
2020-06-12 23:09:43 -0700

    creating array for special characters to be selected this includes numbers and special characters and upper case and lowercase numbers

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,10 @@
+
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// length of at Password at least 8 characters and no more than 128 characters containing lowercase, uppercase, numeric, and/or special characters
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
