268e479993f2fd5321d290816fea4e6675b0fd9e
Lizette Leon <lizetteleon@Lizettes-Air.attlocal.net>
2020-06-12 23:12:19 -0700

    prompt for asking users to select how long they want password to be from 8 to 128 characters long

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -8,3 +8,15 @@ var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+// Ask user the character length for the password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  
